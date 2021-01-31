40f4b90d010fc41b632ca8aa4dba6561a40e6c8a
Ori Markowitz <ori.markowitz@gmail.com>
2021-01-31 18:29:54 -0500

    commit tree 80e9f6683d6d9fcf5234f2373d01cfbb1f15662d

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={user.name} color={user.color} city={user.city}/>
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
